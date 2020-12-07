778e693ee87880358abb98ac294dcc463888e8b0
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-07 23:16:21 +0100

    add constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,8 @@
+export const HOME = '/';
+export const DASHBOARD = '/dashboard';
+export const DASHBOARD_PLANS = '/dashboard/plans';
+export const DASHBOARD_RECIPES = '/dashboard/recipes';
+export const DASHBOARD_SCHEDULES = '/dashboard/schedules';
+export const USER_PROFILE = '/user';
+export const SIGN_IN = '/sign_in';
+export const SIGN_UP = '/sign_up';
