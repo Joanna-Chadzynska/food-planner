29ede8f61b6ddb50ecca261f300b6584ba2e4144
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-08 15:37:08 +0100

    create container: Footer

---
+++ src/containers/Footer/FooterContainer.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface FooterContainerProps {}
+
+const FooterContainer: React.SFC<FooterContainerProps> = () => {
+	return <div className='wrapper'>Footer </div>;
+};
+
+export default FooterContainer;
--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,2 +1,3 @@
+export { default as Footer } from './Footer';
 export { default as HeaderDashboard } from './Header/HeaderDashboard';
 export { default as HeaderHome } from './Header/HeaderHome';
