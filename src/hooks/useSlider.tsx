9a474f8c672d1956af500acc9ccd38779b755cc8
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-09 21:47:30 +0100

    update hook

--- src/hooks/useSlider.tsx
+++ src/hooks/useSlider.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 const useSlider = (query: string) => {
 	const [index, setIndex] = useState(0);
+	const [direction, setDirection] = useState('');
 	const [photos, setPhotos] = useState<Photo[]>([]);
 	const key: any = process.env.REACT_APP_PEXELS_API_KEY;
 	const client = createClient(key);
@@ -26,10 +27,12 @@ const useSlider = (query: string) => {
 		return () => {
 			setPhotos([]);
 		};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	const slideRight = () => {
 		setIndex((index + 1) % photos.length);
+		setDirection('right');
 	};
 
 	const slideLeft = () => {
@@ -39,13 +42,37 @@ const useSlider = (query: string) => {
 		} else {
 			setIndex(nextIndex);
 		}
+		setDirection('left');
 	};
+
+	const jump = (photoIndex: number) => {
+		setIndex(photoIndex);
+
+		if (index < photoIndex) {
+			setDirection('right');
+		} else {
+			setDirection('left');
+		}
+	};
+
+	// useEffect(() => {
+	// 	const interval = setInterval(() => {
+	// 		if (photos.length > 0) {
+	// 			setIndex((prev) => prev + 1);
+	// 		} else {
+	// 			console.log('no photos');
+	// 		}
+	// 	}, 3000);
+	// 	return () => clearInterval(interval);
+	// }, [photos.length]);
 
 	return {
 		index,
 		slideRight,
 		slideLeft,
 		photos,
+		jump,
+		direction,
 	};
 };
 
