a90fb88dc6f615009183dd4cf3623d65d5cea066
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-10 19:22:42 +0100

    update component

--- src/containers/Header/HeaderDashboard.tsx
+++ src/containers/Header/HeaderDashboard.tsx
@@ -2,17 +2,16 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Header, Logo } from 'components';
+import * as ROUTES from 'constants/routes';
 import React, { useEffect, useState } from 'react';
 
 library.add(fas);
-
-export interface HeaderDashboardProps {}
 
 type User = {
 	name: string;
 };
 
-const HeaderDashboard: React.SFC<HeaderDashboardProps> = () => {
+const HeaderDashboard: React.SFC = () => {
 	const [userData, setUserData] = useState<User>();
 	const user = localStorage.getItem('user');
 
@@ -23,21 +22,41 @@ const HeaderDashboard: React.SFC<HeaderDashboardProps> = () => {
 		return () => {};
 	}, [user]);
 
+	const logOut = (name: string | undefined) => {
+		localStorage.removeItem('user');
+		window.location.reload();
+	};
+
 	return (
 		<Header className='dashboard'>
 			<Logo />
 			<Header.ToggleInput type='checkbox' id='nav-toggle' />
 			<Header.Navigation>
-				<Header.Item>
-					<Header.AnchorLink href='#'>
-						<span>{user ? userData?.name : ''}</span>
-						<FontAwesomeIcon
-							icon={['fas', 'user-circle']}
-							size='2x'
-							color='#ff6600'
-						/>
-					</Header.AnchorLink>
-				</Header.Item>
+				<Header.Profile>
+					<Header.Group>
+						<Header.Link to={ROUTES.USER_PROFILE}>
+							{user ? userData?.name : ''}
+						</Header.Link>
+						<Header.Picture>
+							<FontAwesomeIcon
+								icon={['fas', 'user-circle']}
+								size='2x'
+								color='#ff6600'
+							/>
+						</Header.Picture>
+					</Header.Group>
+					<Header.Dropdown>
+						<Header.Group>
+							<Header.Link to={ROUTES.USER_PROFILE}>Profil</Header.Link>
+						</Header.Group>
+						<br />
+						<Header.Group>
+							<Header.Button onClick={() => logOut(userData?.name)}>
+								Wyloguj się
+							</Header.Button>
+						</Header.Group>
+					</Header.Dropdown>
+				</Header.Profile>
 			</Header.Navigation>
 			<Header.ToggleLabel htmlFor='nav-toggle'>
 				<span></span>
