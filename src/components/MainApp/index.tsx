db4f5fea16fe2204d6f1c9bff8aa85edc2565283
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-10 16:14:03 +0100

    update styles

--- src/components/MainApp/index.tsx
+++ src/components/MainApp/index.tsx
@@ -1,10 +1,78 @@
 import React from 'react';
-import { Container } from './styles/mainApp';
+import { Container, Content, Inner } from './styles/mainApp';
 
 export interface MainAppProps {}
+export interface InnerProps {
+	width?: string;
+	height?: string;
+	direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+	content?:
+		| 'flex-start'
+		| 'flex-end'
+		| 'center'
+		| 'stretch'
+		| 'space-between'
+		| 'space-around'
+		| 'space-evenly'
+		| 'baseline';
+	align?:
+		| 'flex-start'
+		| 'flex-end'
+		| 'center'
+		| 'stretch'
+		| 'space-between'
+		| 'space-around'
+		| 'space-evenly'
+		| 'baseline';
+	top?: string;
+}
 
-const MainApp: React.SFC<MainAppProps> = ({ children, ...restProps }) => {
+export interface MainAppComposition {
+	Inner: React.FC<InnerProps>;
+	Content: React.FC<InnerProps>;
+}
+
+const MainApp: React.SFC<MainAppProps> & MainAppComposition = ({
+	children,
+	...restProps
+}) => {
 	return <Container {...restProps}>{children}</Container>;
 };
+
+export const MainAppInner: React.FC<InnerProps> = ({
+	children,
+	width,
+	height,
+	...restProps
+}) => (
+	<Inner width={width} height={height} {...restProps}>
+		{children}
+	</Inner>
+);
+
+export const MainAppContent: React.FC<InnerProps> = ({
+	children,
+	width,
+	height,
+	direction,
+	content,
+	align,
+	top,
+	...restProps
+}) => (
+	<Content
+		width={width}
+		height={height}
+		content={content}
+		direction={direction}
+		align={align}
+		top={top}
+		{...restProps}>
+		{children}
+	</Content>
+);
+
+MainApp.Inner = MainAppInner;
+MainApp.Content = MainAppContent;
 
 export default MainApp;
--- src/components/MainApp/styles/mainApp.ts
+++ src/components/MainApp/styles/mainApp.ts
@@ -1,11 +1,65 @@
-import styled from 'styled-components/macro';
+import styled, { css } from 'styled-components/macro';
+
+export interface InnerProps {
+	width?: string;
+	height?: string;
+	direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+	content?:
+		| 'flex-start'
+		| 'flex-end'
+		| 'center'
+		| 'stretch'
+		| 'space-between'
+		| 'space-around'
+		| 'space-evenly'
+		| 'baseline';
+	align?:
+		| 'flex-start'
+		| 'flex-end'
+		| 'center'
+		| 'stretch'
+		| 'space-between'
+		| 'space-around'
+		| 'space-evenly'
+		| 'baseline';
+	top?: string;
+}
 
 export const Container = styled.article`
 	background-image: url('https://trello-attachments.s3.amazonaws.com/5d78d22236ac1e52efe3c181/5d78d22236ac1e52efe3c1f4/500x500/6bcde9e9d7f8f1d3df331c5bcd9b2a4a/image.png');
 	background-size: contain;
 	min-height: calc(100vh - 250px + 100px);
+	height: 100%;
 	@media screen and (min-width: 800px) {
 		margin-top: 0;
 		min-height: calc(100vh - 250px);
 	}
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Inner = styled.div<InnerProps>`
+	background-color: ${({ theme }) => theme.colors.backgroundScreens};
+	border: 1px dashed ${({ theme }) => theme.colors.sidebarNav};
+	width: ${({ width }) =>
+		css`
+			${width}%
+		`};
+	height: ${({ height }) =>
+		css`
+			${height}%
+		`};
+`;
+
+export const Content = styled.div<InnerProps>`
+	display: flex;
+	flex-direction: ${({ direction }) => direction && direction};
+	justify-content: ${({ content }) => content && content};
+	text-align: ${({ align }) => align && align};
+	height: 100%;
+	padding-top: ${({ top }) =>
+		css`
+			${top}%
+		`};
 `;
