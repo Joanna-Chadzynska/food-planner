400ede32f84eca23758fb11137d764a6d1e19131
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-10 17:49:18 +0100

    update compoent

--- src/containers/Header/HeaderDashboard.tsx
+++ src/containers/Header/HeaderDashboard.tsx
@@ -2,27 +2,40 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Header, Logo } from 'components';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 library.add(fas);
 
 export interface HeaderDashboardProps {}
 
+type User = {
+	name: string;
+};
+
 const HeaderDashboard: React.SFC<HeaderDashboardProps> = () => {
+	const [userData, setUserData] = useState<User>();
+	const user = localStorage.getItem('user');
+
+	useEffect(() => {
+		if (user?.length && typeof user === 'string') {
+			setUserData(JSON.parse(user));
+		}
+		return () => {};
+	}, [user]);
+
 	return (
 		<Header className='dashboard'>
 			<Logo />
 			<Header.ToggleInput type='checkbox' id='nav-toggle' />
 			<Header.Navigation>
 				<Header.Item>
-					<Header.AnchorLink href='#contact'>
-						<span>Imię</span>{' '}
+					<Header.AnchorLink href='#'>
+						<span>{user ? userData?.name : ''}</span>
 						<FontAwesomeIcon
 							icon={['fas', 'user-circle']}
-							size='3x'
+							size='2x'
 							color='#ff6600'
 						/>
-						<i className='fa fa-user-circle-o' aria-hidden='true'></i>
 					</Header.AnchorLink>
 				</Header.Item>
 			</Header.Navigation>
