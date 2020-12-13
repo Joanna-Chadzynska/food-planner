c0fbc1f00b62133354b3e06d33ab9465e53cf292
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-13 23:31:55 +0100

    update button dashboard styles

--- src/components/DashboardButton/index.tsx
+++ src/components/DashboardButton/index.tsx
@@ -1,42 +1,83 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Container, Group, Icon, Inner, Text } from './styles/dashboardButton';
+import {
+	Container,
+	Group,
+	GroupWrapper,
+	Icon,
+	Inner,
+	Text,
+} from './styles/dashboardButton';
 
 export interface DashboardButtonProps {
-	onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
-	id: string;
+	onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+	id?: string;
+	direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+	color?: 'success' | 'info' | 'warning' | 'error';
+}
+
+export interface TextProps {
+	color?: 'success' | 'info' | 'warning' | 'error';
+}
+
+export interface GroupProps {
+	direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
 }
 
 export interface DashboardButtonComposition {
-	Group: React.FC;
+	Group: React.FC<GroupProps>;
+	GroupWrapper: React.FC;
+	Icon: React.FC;
+	Text: React.FC<TextProps>;
 }
 
 library.add(far);
 
 const DashboardButton: React.SFC<DashboardButtonProps> &
-	DashboardButtonComposition = ({ onClick, id, children, ...restProps }) => {
+	DashboardButtonComposition = ({
+	direction,
+	onClick,
+	id,
+	children,
+	color,
+	...restProps
+}) => {
 	return (
 		<Container data-id={id} onClick={onClick} {...restProps}>
-			<Inner>
-				<Icon>
-					<FontAwesomeIcon
-						icon={['far', 'plus-square']}
-						size='6x'
-						color='#468966'
-					/>
-				</Icon>
-				<Text>{children}</Text>
+			<Inner color={color} direction={direction}>
+				{children}
 			</Inner>
 		</Container>
 	);
 };
 
-export const DashboardButtonGroup: React.FC = ({ children, ...restProps }) => (
-	<Group {...restProps}>{children}</Group>
+export const DashboardButtonIcon: React.FC = ({ children, ...restProps }) => (
+	<Icon {...restProps}>{children}</Icon>
 );
 
+export const DashboardButtonGroup: React.FC<GroupProps> = ({
+	direction,
+	children,
+	...restProps
+}) => (
+	<Group direction={direction} {...restProps}>
+		{children}
+	</Group>
+);
+
+export const DashboardButtonText: React.FC = ({ children, ...restProps }) => (
+	<Text {...restProps}>{children}</Text>
+);
+
+export const DashboardButtonGroupWrapper: React.FC = ({
+	children,
+	...restProps
+}) => <GroupWrapper {...restProps}>{children}</GroupWrapper>;
+
 DashboardButton.Group = DashboardButtonGroup;
+DashboardButton.Icon = DashboardButtonIcon;
+DashboardButton.Text = DashboardButtonText;
+DashboardButton.GroupWrapper = DashboardButtonGroupWrapper;
 
 export default DashboardButton;
--- src/components/DashboardButton/styles/dashboardButton.ts
+++ src/components/DashboardButton/styles/dashboardButton.ts
@@ -1,26 +1,61 @@
 import styled from 'styled-components/macro';
 
+export type ButtonProps = {
+	direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+	color?: 'success' | 'info' | 'warning' | 'error';
+};
+
+export type GroupProps = {
+	direction?: 'row' | 'column' | 'row-reverse' | 'column-reverse';
+};
+
 export const Container = styled.button`
 	border: 1px dashed ${({ theme }) => theme.colors.txtRow_extend};
 	background-color: ${({ theme }) => theme.colors.backgroundScreens};
-	padding: 3em 1em;
+	padding: 1em 1.2em;
 `;
 
-export const Group = styled.div`
+export const Group = styled.div<GroupProps>`
 	display: flex;
-	gap: 3em;
+	gap: 2em;
+	flex-direction: ${({ direction }) => direction && direction};
 `;
 
-export const Inner = styled.div`
+export const GroupWrapper = styled.div`
 	display: flex;
-	flex-direction: column;
-	gap: 1em;
+	justify-content: space-evenly;
+	width: 90%;
+	gap: 2em;
 `;
 
 export const Icon = styled.div``;
 
-export const Text = styled.p`
+export const Text = styled.p<ButtonProps>`
 	color: ${({ theme }) => theme.colors.success};
 	font-size: clamp(1rem, 5vw, 1.5rem);
 	font-weight: 600;
+`;
+
+export const Inner = styled.div<ButtonProps>`
+	display: flex;
+	flex-direction: ${({ direction }) => direction && direction};
+	gap: 1em;
+	align-items: center;
+
+	${Text}, ${Icon} > svg {
+		color: ${({ color, theme }) => {
+			switch (color) {
+				case 'success':
+					return `${theme.colors.success}`;
+				case 'warning':
+					return `${theme.colors.warning}`;
+				case 'info':
+					return `${theme.colors.info}`;
+				case 'error':
+					return `${theme.colors.trash}`;
+				default:
+					return `${theme.colors.txtCol_header}`;
+			}
+		}};
+	}
 `;
