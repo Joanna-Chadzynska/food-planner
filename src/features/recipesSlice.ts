eaee4ce28515fee218315f6b0ab177420d36201e
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-16 22:09:39 +0100

    update reducers

--- src/features/recipesSlice.ts
+++ src/features/recipesSlice.ts
@@ -16,13 +16,15 @@ export const recipesSlice = createSlice({
 		importRecipes: (state, { payload }: PayloadAction<Recipe[]>) => {
 			state.recipes = payload;
 		},
-		decrement: (state) => {},
-		// Use the PayloadAction type to declare the contents of `action.payload`
+		importRecipeById: (state, { payload }: PayloadAction<Recipe>) => {
+			state.recipe = payload;
+		},
+
 		incrementByAmount: (state, action: PayloadAction<number>) => {},
 	},
 });
 
-export const { importRecipes } = recipesSlice.actions;
+export const { importRecipes, importRecipeById } = recipesSlice.actions;
 
 // The function below is called a thunk and allows us to perform async logic. It
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
--- src/features/schedulesSlice.ts
+++ src/features/schedulesSlice.ts
@@ -15,24 +15,19 @@ export const schedulesSlice = createSlice({
 	reducers: {
 		importSchedules: (state, { payload }: PayloadAction<Schedule[]>) => {
 			state.schedules = payload;
+			state.loading = false;
+			state.error = null;
 		},
-		decrement: (state) => {},
-		// Use the PayloadAction type to declare the contents of `action.payload`
+		filterSchedulesById: (state, { payload }: PayloadAction<Schedule>) => {
+			state.schedules = state.schedules.filter(
+				(item) => item.id === payload.id
+			);
+		},
 		incrementByAmount: (state, action: PayloadAction<number>) => {},
 	},
 });
 
-export const { importSchedules } = schedulesSlice.actions;
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-// export const incrementAsync = (amount: number): AppThunk => (dispatch) => {
-// 	setTimeout(() => {
-// 		dispatch(incrementByAmount(amount));
-// 	}, 1000);
-// };
+export const { importSchedules, filterSchedulesById } = schedulesSlice.actions;
 
 export const selectSchedules = (state: RootState) => state.schedules.schedules;
 export const selectSchedule = (state: RootState) => state.schedules.schedule;
