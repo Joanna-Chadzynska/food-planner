ba800d04dc692c00ddaf649487daa4b4caf26605
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-17 23:10:35 +0100

    import font awesome libraries

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,11 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routing from './routing/Routing';
+
+library.add(far, fas);
 
 const App = () => {
 	return (
