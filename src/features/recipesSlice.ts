26c2c6bb4f4b3ce0635112dd5443d5d188ba67cc
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-18 20:05:13 +0100

    update recipesSlice

--- src/features/recipesSlice.ts
+++ src/features/recipesSlice.ts
@@ -4,7 +4,12 @@ import { Recipe, RecipesState } from 'models/interfaces/Recipe';
 
 const initialState: RecipesState = {
 	recipes: [],
-	recipe: {} as Recipe,
+	recipe: {
+		name: '',
+		description: '',
+		steps: [],
+		ingredients: [],
+	},
 	loading: true,
 	error: null,
 };
@@ -21,22 +26,21 @@ export const recipesSlice = createSlice({
 		importRecipeById: (state, { payload }: PayloadAction<Recipe>) => {
 			state.recipe = payload;
 		},
-
-		incrementByAmount: (state, action: PayloadAction<number>) => {},
+		addRecipe: (state, { payload }: PayloadAction<Recipe>) => {
+			const { name, description, steps, ingredients } = payload;
+			state.recipe.steps = steps;
+			state.recipe.ingredients = ingredients;
+			state.recipe.name = name;
+			state.recipe.description = description;
+		},
 	},
 });
 
-export const { importRecipes, importRecipeById } = recipesSlice.actions;
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-// export const incrementAsync = (amount: number): AppThunk => (dispatch) => {
-// 	setTimeout(() => {
-// 		dispatch(incrementByAmount(amount));
-// 	}, 1000);
-// };
+export const {
+	importRecipes,
+	importRecipeById,
+	addRecipe,
+} = recipesSlice.actions;
 
 export const selectRecipes = (state: RootState) => state.recipes.recipes;
 export const selectRecipe = (state: RootState) => state.recipes.recipe;
