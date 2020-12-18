be625268cd18d2ee93d9d51e7700860d3da3e7fe
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-18 20:19:42 +0100

    remove unused vars

--- src/containers/Plans/components/Recipe.tsx
+++ src/containers/Plans/components/Recipe.tsx
@@ -1,33 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { RootState } from 'app/store';
 import { Table } from 'components';
-import { importRecipeById } from 'features/recipesSlice';
-import { useHttpClient } from 'hooks';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 export interface RecipeProps {
 	recipe: number;
 }
 
 const Recipe: React.SFC<RecipeProps> = ({ recipe }) => {
-	const client = useHttpClient();
-	const dispatch = useDispatch();
 	const recipes = useSelector((state: RootState) =>
 		state.recipes.recipes.filter((item) => item.id === recipe)
 	);
 
 	const recipeDetails = { ...recipes[0] };
-
-	const getRecipeDetails = async (): Promise<void> => {
-		const response = await client.getRecipeById(recipe);
-		dispatch(importRecipeById(response));
-	};
-
-	useEffect(() => {
-		getRecipeDetails();
-		return () => {};
-	}, []);
 
 	return <Table.ColBody>{recipeDetails.name}</Table.ColBody>;
 };
