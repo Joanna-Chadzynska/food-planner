47a286b70b109a74c79454f6baf5a8eabbc040ae
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-20 19:37:07 +0100

    create new hooks

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,6 @@
 export { default as useForm } from './useForm';
 export { default as useHttpClient } from './useHttpClient';
+export { default as useKeyPress } from './useKeyPress';
 export { default as useModal } from './useModal';
+export { default as useOnClickOutside } from './useOnClickOutside';
 export { default as useSlider } from './useSlider';
