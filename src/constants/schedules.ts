bd4bf5f0f9a40a6dcf2ed6233dbddc5531903303
Joanna Chądzyńska <jmalek87@gmail.com>
2020-12-20 22:14:19 +0100

    create new constants for schedule

---
+++ src/constants/schedules.ts
@@ -0,0 +1,19 @@
+const meals = [
+	{ id: 'breakfast', name: 'śniadanie' },
+	{ id: 'lunch', name: 'drugie śniadanie' },
+	{ id: 'soup', name: 'zupa' },
+	{ id: 'dinner', name: 'drugie danie' },
+	{ id: 'supper', name: 'kolacja' },
+];
+
+const days = [
+	{ id: 'monday', name: 'poniedziałek' },
+	{ id: 'tuesday', name: 'wtorek' },
+	{ id: 'wednesday', name: 'środa' },
+	{ id: 'thursday', name: 'czwartek' },
+	{ id: 'friday', name: 'piątek' },
+	{ id: 'saturday', name: 'sobota' },
+	{ id: 'sunday', name: 'niedziela' },
+];
+
+export { meals, days };
